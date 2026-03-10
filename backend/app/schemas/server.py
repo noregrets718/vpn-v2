c00767860417961bd288aae8a15f92b848d3934a
Noregrets718 <yuriy.nedbailo0220@gmail.com>
2026-03-10 20:52:15 +0700

    change serverCreate schema(remove unused fields

--- backend/app/schemas/server.py
+++ backend/app/schemas/server.py
@@ -6,10 +6,6 @@ from pydantic import BaseModel
 
 class ServerCreate(BaseModel):
     name: str
-    ip_address: str | None = None
-    port: int | None = None
-    username: str | None = None
-    password: str | None = None
     country: str
     city: str | None = None
     port_range_start: int = 10001
