17eb868df263d9c2d671401dbb3c4e01768e2359
Noregrets718 <yuriy.nedbailo0220@gmail.com>
2026-03-11 19:29:24 +0700

    change access_key model(remove unique constraint and add unique for every server)

--- backend/app/models/access_key.py
+++ backend/app/models/access_key.py
@@ -5,15 +5,17 @@ from app.database import Base
 from sqlalchemy import BigInteger, Boolean, DateTime, ForeignKey, Integer, String, func
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import UniqueConstraint
 
 
 class AccessKey(Base):
     __tablename__ = "access_keys"
+    __table_args__ = (UniqueConstraint("server_id", "ss_port"),)
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
     server_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("servers.id"), nullable=False)
-    ss_port: Mapped[int] = mapped_column(Integer, unique=True, nullable=False)
+    ss_port: Mapped[int] = mapped_column(Integer, nullable=False)
     ss_password: Mapped[str] = mapped_column(String(255), nullable=False)
     ss_method: Mapped[str] = mapped_column(String(50), default="chacha20-ietf-poly1305", nullable=False)
     traffic_up: Mapped[int] = mapped_column(BigInteger, default=0, nullable=False)
